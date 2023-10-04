b99872827f7a482588682211b064f6939d0d7114
James Hardy <jimmehmail@gmail.com>
2023-10-04 14:25:52 -0500

    Added flag handling
    Output is commented out, we can uncomment it when file writing is ready

--- team15_project1.go
+++ team15_project1.go
@@ -2,32 +2,42 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"project1/binaryConvert"
 )
 
 func main() {
-	InputFileName := "test1_bin.txt"
+	InputFileName := flag.String("i", "", "Gets the input file name")
+	//OutputFileName := flag.String("o", "", "Gets the output file name")
+
+	flag.Parse()
+
+	if flag.NArg() != 0 {
+		os.Exit(200)
+	}
+
+	fmt.Println(InputFileName)
+	fmt.Println(*InputFileName)
+	//InputFileName := "test1_bin.txt"
 	//OutputFileName := "team15_out"
 
-	inputFile, err := os.Open(InputFileName)
+	inputFile, err := os.Open(*InputFileName)
 	if err != nil {
 		panic(err)
 	}
 
 	scanner := bufio.NewScanner(inputFile)
 
-
 	programCounter := 96
-  
 	//Start reading instructions
 	for scanner.Scan() {
 		line := scanner.Text()
 		opcode := binaryConvert.BinaryStringToInt(line[:11])
 		opcodeString := binaryConvert.IntToInstruction(opcode)
 		insType := binaryConvert.GetInstructionType(opcodeString)
-    
+
 		//TODO
 		//Put file write statements into this switch
 		//James H: I added some print statements just to demo how to use my package
@@ -35,14 +45,14 @@ func main() {
 		case "R":
 			//rm, rn, rd etc are labels given in the lecture 7 slides
 			rm := line[11:16]
-			shamt := line[16:22]
+			//shamt := line[16:22]
 			rn := line[22:27]
 			rd := line[27:32]
 			fmt.Printf("%s  R%d, R%d, R%d\n", opcodeString, binaryConvert.BinaryStringToInt(rd), binaryConvert.BinaryStringToInt(rn), binaryConvert.BinaryStringToInt(rm))
 		case "I":
-			immediate := line[10:22]
-			rn := line[22:27]
-			rd := line[27:32]
+			//immediate := line[10:22]
+			//rn := line[22:27]
+			//rd := line[27:32]
 		case "IM":
 		case "CB":
 		case "B":
